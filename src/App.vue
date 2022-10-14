25abc9b01aeb7a2450b0f35f33470ed0e15d2a55
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-10-14 09:45:11 +0600

    add my skills using iconify

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <SiteHeader/>
+    <MySkills/>
     <Education />
     <ContactMe />
     <!-- <router-view /> -->
@@ -11,11 +12,12 @@
 <script>
 import ContactMe from './components/ContactMe.vue';
 import Education from './components/Education.vue';
+import MySkills from './components/MySkills.vue';
 import SiteFooter from './components/SiteFooter.vue';
 import SiteHeader from './components/SiteHeader.vue';
 
 export default {
-    components: { SiteHeader, SiteFooter, ContactMe, Education }
+    components: { SiteHeader, SiteFooter, ContactMe, Education, MySkills }
 }
 </script>
 
